24fd3924608268635a7385dcc7ebd30627f80875
Joynal Abedin <joynal.9933@gmail.com>
2023-05-27 07:33:27 -0700

    initial commit

---
+++ App1/index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+
+app = express();
+
+
+app.get('/', function(req, res){
+    res.send("Hello express js")
+});
+
+
+app.listen(8000, function(){
+    console.log('Server Run success');
+}) 
